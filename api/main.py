cdbe1f88730b503951a3f85bb5d1ce65ed703deb
jacksonsr451 <jacksonsr451@gmail.com>
2024-11-26 19:54:46 -0300

    chore: setting run server

--- api/main.py
+++ api/main.py
@@ -1,10 +1,15 @@
+from uvicorn import run
+
 from fastapi import FastAPI
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
+from .config.settings import settings
 from .routes import init_routes
 
 from .middlewares.jwt_middleware import JWTMiddleware
 from .middlewares.rate_limit_middleware import RateLimitMiddleware
+
+settings.configure_logging()
 
 app = FastAPI(title='Api Blog', description="Api for management to a blog app with python and FastAPI", version='1.0.0')
 
@@ -13,3 +18,7 @@ app.add_middleware(RateLimitMiddleware)
 app.add_middleware(JWTMiddleware)
 
 init_routes(app=app)
+
+
+def create_app() -> None:
+    run("api.main:app", host=settings.SERVER_HOST, port=settings.SERVER_PORT, reload=settings.SERVER_RELOAD)
